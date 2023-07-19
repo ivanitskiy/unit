690945bb05371948979c090c587f61e705cd16a4
Maxim Ivanitskiy <m.ivanitskiy@f5.com>
2023-07-18 17:08:20 -0700

    fixed issues with build.rs

--- wasm-demo/rust/unit-wasm-sys/build.rs
+++ wasm-demo/rust/unit-wasm-sys/build.rs
@@ -2,31 +2,36 @@ use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
 
+    let out_dir_env = std::env::var("OUT_DIR").expect("The required environment variable OUT_DIR was not set");
+    let out_path = std::path::PathBuf::from(out_dir_env);
 
-    // println!("cargo:rustc-link-search=native=../../wasm-demo/src");
-    // Tell cargo to look for shared libraries in the specified directory
-    println!("cargo:rustc-link-search=../../wasm-demo/src");
+    // Set the path to the libunit-wasm.o and unit-wasm.o files
 
-    println!("cargo:rustc-link-search=native=../../src/");
-    println!("cargo:rustc-link-lib=static=unit-wasm");
+    // TODO: build these .o files here and don't rely on make to compile these...
+    // run those `make unit-wasm.o  && make libunit-wasm.o`. this build.rs doesn't deal with this now
+    // for now just lazy step
+    let libunit_wasm_path = "../../src/libunit-wasm.o";
+    let unit_wasm_path = "../../src/unit-wasm.o";
 
-    // Tell cargo to invalidate the build.rs changes
+    println!("cargo:rustc-link-search=native={}", libunit_wasm_path);
+    println!("cargo:rustc-link-search=native={}", unit_wasm_path);
+
+
+    // Tell cargo to invalidate the build.rs changes of
     println!("cargo:rerun-if-changed=build.rs");
-
-    // Tell cargo to invalidate the built crate whenever the wrapper changes
     println!("cargo:rerun-if-changed=wrapper.h");
+
     let bindings = bindgen::Builder::default()
         .allowlist_function("^luw_.*")
         .allowlist_var("^luw_.*")
         .allowlist_type("^luw_.*")
-        .header("wrapper.h")
+        .clang_arg(format!("-L{}", out_path.display()))
+        .header("../../src/libunit-wasm.h")
         .layout_tests(false)
         .generate()
         .expect("Unable to generate bindings");
     // Write the bindings to the $OUT_DIR/bindings.rs file.
-    let out_dir_env =
-        std::env::var("OUT_DIR").expect("The required environment variable OUT_DIR was not set");
-    let out_path = std::path::PathBuf::from(out_dir_env);
+
     bindings
         .write_to_file(out_path.join("bindings.rs"))
         .expect("Couldn't write bindings!");
